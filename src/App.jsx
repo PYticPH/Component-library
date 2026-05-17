f78068b699ff3fba96e7136b33a7ccdb3d9f48da
PYticPH <pyticph@gmail.com>
2026-05-17 22:40:57 +0200

    update: add banner component to root app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client"
 import Badge from "./components/badge/index.jsx"
+import Banner from "./components/banner/index.jsx"
 
 //Badge variant = (square | pill)
 //Badge colors = (gray|red|yellow|green|blue|indigo|purple|pink)
@@ -13,7 +14,24 @@ function App() {
           <Badge.Text>Admin</Badge.Text>
         </Badge.Shape>
       </Badge>
+      <Banner variant='success'>
+        <Banner.BannerTitle>Congratulation!</Banner.BannerTitle>
+        <Banner.BannerText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.BannerText>
+      </Banner>
+      <Banner variant='warning'>
+        <Banner.BannerTitle>Attention!</Banner.BannerTitle>
+        <Banner.BannerText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.</Banner.BannerText>
+      </Banner>
+      <Banner variant='error'>
+        <Banner.BannerTitle>There is a problem with your application</Banner.BannerTitle>
+        <Banner.BannerText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.</Banner.BannerText>
+      </Banner>
+      <Banner variant='info'>
+        <Banner.BannerTitle>Upadate availabe</Banner.BannerTitle>
+        <Banner.BannerText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.BannerText>
+      </Banner>
     </div>
+
   )
 }
 
