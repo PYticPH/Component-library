87d55e033efbf02d60ca37fe51faca11bbc54801
PYticPH <pyticph@gmail.com>
2026-05-18 18:15:38 +0100

    update: include card component to app root

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client"
 import Badge from "./components/badge/index.jsx"
 import Banner from "./components/banner/index.jsx"
+import Card from "./components/card/index.jsx"
 
 //Badge variant = (square | pill)
 //Badge colors = (gray|red|yellow|green|blue|indigo|purple|pink)
@@ -30,6 +31,10 @@ function App() {
         <Banner.BannerTitle>Upadate availabe</Banner.BannerTitle>
         <Banner.BannerText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.BannerText>
       </Banner>
+      <Card>
+        <Card.Title>Easy Deployment</Card.Title>
+        <Card.Text>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</Card.Text>
+      </Card>
     </div>
 
   )
